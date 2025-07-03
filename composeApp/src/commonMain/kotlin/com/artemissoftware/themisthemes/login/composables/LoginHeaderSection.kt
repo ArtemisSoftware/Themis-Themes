64c59301c5669ad74cd7188ae184a1c53966c7b5
gustavo <gustavo.isaac@gmail.com>
2025-07-03 10:47:22 +0100

    LoginHeaderSection

---
+++ composeApp/src/commonMain/kotlin/com/artemissoftware/themisthemes/login/composables/LoginHeaderSection.kt
@@ -0,0 +1,29 @@
+package com.artemissoftware.themisthemes.login.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+internal fun LoginHeaderSection(
+    alignment: Alignment.Horizontal = Alignment.Start,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = alignment
+    ) {
+        Text(
+            text = "Log In",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "Capture your thoughts and ideas.",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
